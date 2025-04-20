bdf3c213f0ccb35143333ef51dfcb82ddbd85c40
dom_Ryan <ryanrodriguues06@gmail.com>
2025-04-20 18:47:03 -0300

    feat: Adiciona mais metodos ao nosso serviço da API do WAHA

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify
-
+import time
 from services.waha import Waha
 
 app = Flask(__name__)
@@ -11,14 +11,24 @@ def webhook():
     waha = Waha()
 
     chat_id = data['payload']['from']
-    
-    print(f"Esse numero mandou mensagem: {chat_id}")
+    mensagem = data['payload']['body']
+    checagem: bool = chat_id.endswith("@c.us")
 
-    if "@c" in chat_id:
+    print(f"Mensagem: {mensagem} evianda por esse número: {chat_id}")
+
+    """Verificando se a mensagem que recebemos não vem de um grupo"""
+    if checagem:
+        waha.start_typing(chat_id=chat_id)
+
+        time.sleep(4)
+
         waha.send_message(
             chat_id=chat_id,
-            message="Automatico",
+            message="""Olá! 😊 Seja bem-vindo(a) a GiSalgados. 
+Se precisar de ajuda, tiver dúvidas ou quiser conhecer mais sobre nossos serviços, é só nos chamar.""",
         )
+
+        waha.stop_typing(chat_id=chat_id)
 
     return jsonify({'status': 'sucess'}), 200
 
--- services/waha.py
+++ services/waha.py
@@ -1,24 +1,81 @@
 import requests
+import os
+
 
 class Waha:
 
     def __init__(self):
         self.__api_url = "http://waha:3000"
 
-    def send_message(self, chat_id, message):
+    def send_message(self, chat_id: str, message: str) -> dict:
+        """ Envia uma mensagem para um chat via API."""
         url = f"{self.__api_url}/api/sendText"
-
         headers = {
-            'Context-Type': "application/json",
+            'Content-Type': "application/json",
         }
         payload = {
             'session': 'default',
             'chatId': chat_id,
             'text': message,
         }
+        try:
+           response = requests.post(
+                            url=url,
+                            json=payload,
+                            headers=headers
+                        )
+           response.raise_for_status()
+           
+           return response.json()
+        except requests.exceptions.RequestException as ex:
+            return {"error": str(ex)}
 
-        requests.post(
-            url=url,
-            json=payload,
-            headers=headers
-        )
+
+    def start_typing(self, chat_id: str) -> dict:
+        """Envia um estado de 'digitando' ao Chat"""
+        url = f"{self.__api_url}/api/startTyping"
+        
+        headers = {
+            'Content-Type': 'application/json',
+        }
+        payload = {
+            'session': 'default',
+            'chatId': chat_id,
+        } 
+
+        try:
+            response = requests.post(
+                url=url,
+                json=payload,
+                headers=headers,
+            )
+            
+            response.raise_for_status()
+
+            return response.json()
+        
+        except requests.RequestException as ex:
+            return {"error": str(ex)}
+
+    def stop_typing(self, chat_id: str) -> dict:
+        """Para de atualizar o estado 'digitando' ao chat"""
+        url = f"{self.__api_url}/api/stopTyping"
+        headers = {
+            'Content-Type': 'application/json',
+        }
+        payload = {
+            'session': 'default',
+            'chatId': chat_id,
+        }
+
+        try:
+            response = requests.post(
+                url=url,
+                json=payload,
+                headers=headers,
+            )
+            response.raise_for_status()
+
+            return response.json()
+        except requests.RequestException as ex:
+            return {"error": str(ex)}
