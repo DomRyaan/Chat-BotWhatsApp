6aac30ab2b0b9aca7e45b2312b82225063208ba2
dom_Ryan <ryanrodriguues06@gmail.com>
2025-04-21 19:05:08 -0300

    feat: Melhorando legibilidade do código

--- app.py
+++ app.py
@@ -22,29 +22,39 @@ def webhook():
     chat_id = data['payload']['from']
     received_message = data['payload']['body']
 
+    is_group = "@g.us" in chat_id
+    is_status = "status@broadcast" in chat_id
+
+   # Ignora mensagens de grupos ou atualizações de status
+    if is_group or is_status:
+        return jsonify({"status": "sucess", "mensagem": "Mensagem ignorada: grupo ou status"}), 200
+
+
     fuso_horario = timezone('America/Sao_Paulo')
     horario = datetime.now(fuso_horario).hour
 
-    """Verificando se a mensagem que recebemos não vem de um grupo"""
-    if chat_id.endswith("@c.us"):
-        
-        if rules.is_open(horario):
-            waha.start_typing(chat_id=chat_id)
+    # Verifica se o estabelecimento está fechado
+    if not (rules.is_open(horario)):
 
-            time.sleep(3)
-            
-            response = ai_bot.invoke(question=received_message)
-            waha.send_message(
-                chat_id=chat_id,
-                message=response,
-                )
+        waha.send_message(
+        chat_id=chat_id,
+        message="Desculpe, mas no momento estamos fechados. "
+                "Nosso horário de funcionamento é das 6h às 11h (manhã) "
+                "e das 14h às 18h (tarde).",
+        )
+        return jsonify({"status": "success", "message": "Estabelecimento fechado"}), 200
+    
+    waha.start_typing(chat_id=chat_id)
 
-            waha.stop_typing(chat_id=chat_id)
-        else:
-             waha.send_message(
-                chat_id=chat_id,
-                message="Desculpe, mas no momento estamos fechados. Nosso horário de funcionamento é das 6h às 11h e das 14h às 18h.",
-            )
+    time.sleep(3)
+    
+    response = ai_bot.invoke(question=received_message)
+    waha.send_message(
+        chat_id=chat_id,
+        message=response,
+        )
+
+    waha.stop_typing(chat_id=chat_id)
 
     return jsonify({'status': 'sucess'}), 200
 
